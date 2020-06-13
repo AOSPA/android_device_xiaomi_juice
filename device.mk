66d20d8ff3dd6e4f2eefca8b80178a22d54ebbbd
Jesse Chan <jc@lineageos.org>
2020-06-13 22:45:25 +0800

    juice: Enable zygote unspecialized app process pool
    Change-Id: I6dfcb59dace5eaa7551caeae86db3b485c0cfbf1
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -498,4 +498,5 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Zygote
 PRODUCT_SYSTEM_PROPERTIES += \
-    zygote.critical_window.minute=10
+    zygote.critical_window.minute=10 \
+    persist.device_config.runtime_native.usap_pool_enabled=true
