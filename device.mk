5a73f845173fd4cdb4467276b0d4cb9fa4269ac8
Martin Liu <liumartin@google.com>
2021-07-19 10:02:01 +0000

    juice: props: Add filecache_min_kb tuning for lmkd
    Set filecache_min_kb to 150MB according to our
    test data.
    
    Bug: 193293513
    Change-Id: I99381d9ef024b18249ebb21dc61e4668dbe1cb31
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/input/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# LMK
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.lmk.filecache_min_kb=153600
+    
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
