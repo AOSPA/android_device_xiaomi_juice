e95ceb3c2c38b2031bf448cadeeb3c99f49e93c6
Long Ling <longling@google.com>
2021-07-20 09:52:19 -0700

    juice: Increase active layer timeout to 1s
    Avoid SF cache thrashing in some cases.
    
    Bug: 189928400
    Test: no flicking issue with the testbuld (removed displaycolor fix)
          and flicking happens after set the prop to 0
    
    Change-Id: I24aa79001ec21cff18ae5ac8ff988f70ed121061
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>

--- device.mk
+++ device.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1 \    
+    debug.sf.layer_caching_active_layer_timeout_ms=1000 \
     vendor.display.enable_async_powermode=0 \
     debug.renderengine.backend=skiaglthreaded \
     ro.hwui.render_ahead=20
