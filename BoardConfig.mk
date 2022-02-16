883d44dc01d12b1abff4c757fef62b5781a72b89
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-16 16:07:54 +0530

    juice: power: Adapt DT2W extension to AOSPA power HAL
    https: //github.com/pa-gr/android_vendor_aospa/commit/027c4d9fea9bce9df0b8de6314e03934d97f8652
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I8bb59faca49570705666cb45c14db117a832e324
    Signed-off-by: Alexey Shelby (unleaked.) <lexaxx14@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_juice
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
