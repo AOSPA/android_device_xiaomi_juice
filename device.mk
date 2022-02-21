8cdc8239518cb1483e17df25d4255d56a806ab7c
Martin Liu <liumartin@google.com>
2022-02-21 16:33:28 +0800

    juice: props: Set lmk stall_limit_critical threshold
    To prevent APC watchdog under heavy memory pressure,
    set stall_limit_critical to 40.
    
    Bug: 205182133
    Test: Run ACT under a heavy memory pressure
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: Iaac006d1b1f2fff8a47875e792f6d2179bae80a9
    Merged-In: Iaac006d1b1f2fff8a47875e792f6d2179bae80a9
    (cherry picked from commit 360afd734baac29571b771424013edb6695e8619)
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -244,7 +244,8 @@ PRODUCT_COPY_FILES += \
 # LMK
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lmk.filecache_min_kb=153600 \
-    ro.lmk.kill_timeout_ms=50
+    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.stall_limit_critical=40
     
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
