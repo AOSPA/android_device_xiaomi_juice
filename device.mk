e25292b818dede5ac275bd6d945c55bc1fdb1974
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-18 11:23:02 -0400

    juice: Inherit proprietary-files With qcom/common qti components
    [1] - Stock blobs from lime MIUI V12.5.11.0.RQJMIXM
    [2] - BSP common blobs from LA.UM.9.12.r1-14400-SMxx50.QSSI12.0
    [3] - Camera blobs for citrus from qssi-user-11-RKQ1.201004.002-V12.5.7.0.RJFMIXM-release-keys
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Alexey Shelby <lexaxx14@gmail.com>

--- device.mk
+++ device.mk
@@ -26,11 +26,11 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Boot control HAL
+# Audio
+BOARD_SUPPORTS_OPENSOURCE_STHAL := true 
+
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.audio.service
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -59,6 +59,44 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.11.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
+
+PRODUCT_PACKAGES += \
+    memtrack.bengal \
+    gralloc.bengal \
+    hwcomposer.bengal
+
+PRODUCT_PACKAGES += \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libqdMetaData \
+    libsdmcore \
+    libsdmutils \
+    libtinyxml
+
+PRODUCT_PACKAGES += \
+    libvulkan
+
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
@@ -80,6 +118,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FM
+BOARD_HAVE_QCOM_FM := true
+
 # FUSE passthrough
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.fuse.passthrough.enable=true
@@ -99,6 +140,9 @@ PRODUCT_COPY_FILES += \
 
 # GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
+# GPS
+LOC_HIDL_VERSION := 4.0
 
 # Health
 PRODUCT_PACKAGES += \
@@ -175,6 +219,24 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
+# QC common
+TARGET_BOARD_PLATFORM := bengal
+
+TARGET_COMMON_QTI_COMPONENTS := \
+    audio \
+    av \
+    bt \
+    gps \
+    init \
+    media-legacy \
+    overlay \
+    perf \
+    telephony \
+    usb \
+    vibrator \
+    wfd \
+    wlan
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
@@ -217,3 +279,10 @@ PRODUCT_COPY_FILES += \
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
+
+# Vendor
+$(call inherit-product, vendor/xiaomi/juice/juice-vendor.mk)
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor:32
