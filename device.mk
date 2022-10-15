f305e1aaaf59392a3d49e74f1ddc781ad146b085
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-14 19:50:27 -0500

    juice: Disable SF backpressure
    Avoids jank in the event that frame deadlines
    are not hit.
    
    Change-Id: I01d9db3f18e2e41ef2188201feaf377c70161241
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -156,6 +156,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.enable_rounded_corner=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.disable_client_composition_cache=1 \
     vendor.display.enable_async_powermode=0 \
