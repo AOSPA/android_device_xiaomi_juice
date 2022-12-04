4302dddf2b651948dfecbad648d730e5b6d807e7
Alexey Shelby <lexaxx14@gmail.com>
2022-12-04 11:26:59 +0000

    juice: Build android.hardware.sensors@2.0-service.multihal.
    Signed-off-by: Alexey Shelby <lexaxx14@gmail.com>

--- device.mk
+++ device.mk
@@ -157,6 +157,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
