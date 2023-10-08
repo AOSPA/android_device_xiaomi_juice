ddcc99e12fbfd13ac823a711fceadb4d48fc191f
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-10-08 18:08:04 +0000

    juice: Remove DEXPREOPT_GENERATE_APEX_IMAGE
    Change-Id: Iaf2e0c2add9fdebb808d99c28c4c9fdf84f28312
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@
 DEVICE_PATH := device/xiaomi/juice
 COMMON_PATH := device/qcom/common
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
